167f45fc3a200c0057aa42be2526010332db9a9e
PreetiGalagali <preetigalagali08@gmail.com>
2025-02-01 17:27:31 +0530

    comiting to dev

---
+++ wipro_java_project/src/com/wipro/java/MyClass.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class MyClass {
+
+	int num = 5;
+	
+	public void changeValue(int num) {
+		// TODO Auto-generated constructor stub
+		this.num = num;
+	
+		}
+	public MyClass() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass obj = new MyClass();
+		obj.changeValue(10);
+		System.out.println(obj.num);
+
+	}
+
+}
