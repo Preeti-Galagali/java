49b252d56836fd741957acb504e91773fca6b509
PreetiGalagali <preetigalagali08@gmail.com>
2025-02-05 14:13:11 +0530

    day 4

---
+++ wipro_java_project/src/com/wipro/java/collection/Array.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.wipro.java.collection;
+
+/**
+ * 
+ */
+public class Array {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr;
+		arr = new int[5];
+		
+		
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+		
+		System.out.println("Array elements");
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+}
