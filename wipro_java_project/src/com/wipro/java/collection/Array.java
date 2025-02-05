16146d80f32e3ffa687a31c135e0a6e6a19b845c
PreetiGalagali <preetigalagali08@gmail.com>
2025-02-05 16:03:24 +0530

    day 4

--- wipro_java_project/src/com/wipro/java/collection/Array.java
+++ wipro_java_project/src/com/wipro/java/collection/Array.java
@@ -12,18 +12,21 @@ public class Array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// declaring an array for 5 integers
 		int[] arr;
 		arr = new int[5];
 		
-		
+		// initialization of array elements
 		arr[0] = 10;
 		arr[1] = 20;
 		arr[2] = 30;
 		arr[3] = 40;
 		arr[4] = 50;
 		
+		// printing array elements
 		System.out.println("Array elements");
 		
+		// using for loop printing array elements
 		for(int i=0; i<arr.length; i++) {
 			System.out.println(arr[i]);
 		}
--- wipro_java_project/src/com/wipro/java/collection/MapImplementation.java
+++ wipro_java_project/src/com/wipro/java/collection/MapImplementation.java
@@ -11,27 +11,34 @@ public class MapImplementation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Creating HashMap where integer is key and string is value
 		Map<Integer, String> map = new HashMap<>();
 		
+		// adding key-value pairs to map
 		map.put(1, "Apple");
 		map.put(2, "Banana");
 		map.put(3, "Apple");
 		
+		//accessing values by key
 		System.out.println("Key 1:" + map.get(1));
 		System.out.println("Key 2:" + map.get(2));
 		
+		// checking if key exists in a map
 		if(map.containsKey(3)) {
 			System.out.println("Key 3 exists with value:" + map.get(3));
 			
 		}
 		
+		// iterating over the map using for-each loop
 		System.out.println("Iterating through the map:");
 		for(Map.Entry<Integer, String> entry : map.entrySet()) {
 			System.out.println(entry.getKey() + ": " + entry.getValue());
 		}
 		
+		// removing key-value pair
 		map.remove(2);
 		
+		// iterating over the map using for-each loop
 		System.out.println("Map after removal of key 2:");
 		for(Map.Entry<Integer, String> entry : map.entrySet()) {
 			System.out.println(entry.getKey() + ": " + entry.getValue());
