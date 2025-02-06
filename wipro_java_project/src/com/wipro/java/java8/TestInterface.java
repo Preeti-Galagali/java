eff06936afbc4bcd9a91448cec41b3c9efd2907a
PreetiGalagali <preetigalagali08@gmail.com>
2025-02-06 19:01:02 +0530

    day 5

---
+++ wipro_java_project/src/com/wipro/java/java8/TestInterface.java
@@ -0,0 +1,20 @@
+package com.wipro.java.java8;
+
+public interface TestInterface {
+
+	// default method
+	default void show() {
+		System.out.println("Default Interface");
+	}
+}
+ interface TestInterface2 {
+	 
+	 // default method
+	 default void show() {
+			System.out.println("Default Interface2");
+		}
+ }
+ 
+
+ 
+
